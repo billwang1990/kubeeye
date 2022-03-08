9dabaf3c4547fed471a5b1bbbbbac25ee22dfa0f
yaqing <billwang1990@gmail.com>
2022-03-08 14:57:48 +0800

    remove useless code

--- pkg/kube/getresources.go
+++ pkg/kube/getresources.go
@@ -18,7 +18,6 @@ import (
 	"context"
 	"fmt"
 	"time"
-	// "strings"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/fields"
@@ -60,16 +59,7 @@ func GetK8SResources(ctx context.Context, kubernetesClient *KubernetesClient, na
 	//for _, excludedNamespace := range excludedNamespaces {
 	//	fieldSelectorString += ",metadata.namespace!=" + excludedNamespace
 	//}
-	fmt.Printf("fieldSelectorString is %s", fieldSelectorString)
-	// if namespace != "" {
-	// 	includedNamespaces := strings.Split(namespace, ",")
-	// 	for _, include := range includedNamespaces {
-	// 		fieldSelectorString += ",metadata.namespace=" + include
-	// 	}
-	// }
-	fmt.Printf("fieldSelectorString is  2 %s", fieldSelectorString)
 	fieldSelector, _ := fields.ParseSelector(fieldSelectorString)
-	fmt.Printf("fieldSelector is  ====%s", fieldSelector)
 	listOptsExcludedNamespace := metav1.ListOptions{
 		FieldSelector: fieldSelectorString,
 		LabelSelector: fieldSelector.String(),
